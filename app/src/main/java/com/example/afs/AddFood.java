9e2a02ce27537254c648f910659c53bee381a474
Yinbai Ding <dingyinbai@megalinkware.com>
2019-05-19 15:34:23 -0700

    Need database implementation for the add button under addFood activity -------------------------------------------------------------------------- Added all the functionality for the buttons in DietPlan page(except database)   - Now the plus button leads to FoodHistory page   - Now "ADD NEW ITEM" button leads to AddFood page   - Now "ADD" button will check if the input is good in format       - if name is empty, it does not proceed       - if calorie is negative or contains invalid character, it does not proceed       - If all valid, it returns to the FoodHistory activity

--- app/src/main/java/com/example/afs/AddFood.java
+++ app/src/main/java/com/example/afs/AddFood.java
@@ -1,13 +1,55 @@
 package com.example.afs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddFood extends AppCompatActivity {
+
+    private Button addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.addfood);
+
+        addButton = (Button)findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                add();
+            }
+        });
+    }
+
+    /**
+     * Add the food to the database based on user input
+     */
+    public void add() {
+        EditText foodName = (EditText)findViewById(R.id.foodName);
+        EditText foodCalorie = (EditText)findViewById(R.id.foodCalorie);
+
+        try {
+
+            //make sure the input calorie is valid, otherwise, reset the calorie input box
+            String name = foodName.getText().toString();
+            int calorie = Integer.parseInt(foodCalorie.getText().toString());
+            if(calorie < 0 || name.length() == 0)
+                throw new NumberFormatException();
+
+            //TODO Need to add the food information into database
+
+
+            //After updating database, go back to the previous page
+            finish();
+
+        } catch(NumberFormatException e) {
+            foodCalorie.setText("");
+            return;
+        }
+
+
     }
 }
--- app/src/main/java/com/example/afs/FoodHistory.java
+++ app/src/main/java/com/example/afs/FoodHistory.java
@@ -1,13 +1,28 @@
 package com.example.afs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class FoodHistory extends AppCompatActivity {
-
+    private Button addItemButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.food_history);
+        addItemButton = (Button) findViewById(R.id.addItemButton);
+        addItemButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                addItem();
+            }
+        });
+    }
+
+
+    public void addItem() {
+        Intent intent = new Intent(this, AddFood.class);
+        startActivity(intent);
     }
 }
