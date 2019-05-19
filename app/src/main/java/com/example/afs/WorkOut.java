a53a94da9d318c463c532353b0b0a3eddb5b910d
liyanxun <liyanxun1128@gmail.com>
2019-05-18 17:56:55 -0700

    login added, navBar issue

--- app/src/main/java/com/example/afs/WorkOut.java
+++ app/src/main/java/com/example/afs/WorkOut.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +14,7 @@ public class WorkOut extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.workout, container, false);
+        return inflater.inflate(R.layout.workout, container, true);
 
     }
 
--- app/src/main/java/com/example/afs/login.java
+++ app/src/main/java/com/example/afs/login.java
@@ -1,18 +1,34 @@
 package com.example.afs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class login extends AppCompatActivity {
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        loginButton = (Button) findViewById(R.id.loginbutton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.workout);
+                //signIn();
+            }
+        });
+    }
+
+    public void signIn() {
+        Intent intent = new Intent(this, WorkOut.class);
+        startActivity(intent);
     }
 
 }
