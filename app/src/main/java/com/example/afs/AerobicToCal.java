337fb3c4986265317513ab69720b210fe024be65
HimLi <qqwilliamliqq@gmail.com>
2019-06-03 01:49:35 -0700

    set the tool bar for aerobic exersice

--- app/src/main/java/com/example/afs/AerobicToCal.java
+++ app/src/main/java/com/example/afs/AerobicToCal.java
@@ -2,12 +2,25 @@ package com.example.afs;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class AerobicToCal extends AppCompatActivity {
-
+    private android.support.v7.widget.Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_aerobic);
+
+        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.aerobicToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Aerobic Exercise");
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
